678d85064b578deacc80ab23d0de5d82e9047f17
Sasha <sasha.konyshev1003@gmail.com>
2015-04-14 16:13:08 +0300

    add bad paginator

--- core/views.py
+++ core/views.py
@@ -1,24 +1,54 @@
 from django.shortcuts import render
+from django.http import Http404
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from core.models import Question, Profile, Tag, Answer
 
 N = 10	#Number of questions on page
 
 def index(request):
-	question_list = Question.objects.order_by('-date')[:N]
+	question_list = Question.objects.order_by('-date').all()
+	paginator = Paginator(question_list, N) # Show N contacts per page
+
+	page = request.GET.get('page')
+	try:
+		question_list = paginator.page(page)
+	except PageNotAnInteger:
+		# If page is not an integer, deliver first page.
+		question_list = paginator.page(1)
+	except EmptyPage:
+		# If page is out of range (e.g. 9999), deliver last page of results.
+		question_list = paginator.page(paginator.num_pages)
+
+	# question_list = Question.objects.order_by('-date')[:N]
 	context = {'question_list': question_list}
 	return render(request, 'index.html', context)
 
 def popular(request):
-	question_list = Question.objects.order_by('-rating')[:N]
+	question_list = Question.objects.order_by('-date').all()
+	paginator = Paginator(question_list, N) # Show N contacts per page
+
+	page = request.GET.get('page')
+	try:
+		question_list = paginator.page(page)
+	except PageNotAnInteger:
+		# If page is not an integer, deliver first page.
+		question_list = paginator.page(1)
+	except EmptyPage:
+		# If page is out of range (e.g. 9999), deliver last page of results.
+		question_list = paginator.page(paginator.num_pages)
+
 	context = {'question_list': question_list}
 	return render(request, 'popular.html', context)
 
 #	check nubmer of question
 def question(request, question_id):
-	question = Question.objects.get(id=question_id)
+	try:
+		question = Question.objects.get(id=question_id)
 
-	context = {'question': question}
-	return render(request, 'question.html', context)
+		context = {'question': question}
+		return render(request, 'question.html', context)
+	except Exception, e:
+		raise Http404("Question does not exist")
 
 def tag(request, tag_name):
 	question_list = []
@@ -45,3 +75,4 @@ def base(request):
 
 def ask(request):
     return render(request, 'ask.html')
+
